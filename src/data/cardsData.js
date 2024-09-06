27197feca8358f13f53826de223a8d8c60dd5fd7
Hugo Lemieux <h.lemieux66@gmail.com>
2024-09-06 11:16:34 -0400

    changes lorem ipsum for real data

--- src/data/cardsData.js
+++ src/data/cardsData.js
@@ -3,48 +3,47 @@ export const cardsData = [
     id: "appWeb",
     title: "La création d'applications web personnalisées",
     message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Vous avez besoin d'une application pour automatiser vos processus? Vous avez une idée de nouvelle, mais pas les ressources techniques pour la concrétiser? Je vous offre de bâtir une solution personnalisée selon vos besoins et vos goûts. Vous pouvez arriver avec votre propre design, votre nom de domaine, votre hébergement ou vous pouvez demander une solution clé en main où je m'occuperai de toutes les étapes.",
   },
   {
     id: "siteWeb",
     title: "La création de votre site web",
     message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Vous avez besoin d'un site web ou le vôtre a besoin d'une cure de rajeunissement? Comme pour la création d'application web, j'y vais selon vos besoins. À travers la plateforme WordPress, que ce soit une page simple ou plusieurs pages avec des intégrations à vos outils, votre site sera fait sur mesure.",
   },
-  {
-    id: "ecomm",
-    title: "La création de commerces en ligne",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  },
+  // {
+  //   id: "ecomm",
+  //   title: "La création de commerces en ligne",
+  //   message: "",
+  // },
   {
     id: "rd",
     title: "Appui à la R&D d'une entreprise",
     message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "J'offre la possibilité d'apporter mon aide à votre département de recherche et développement. Je suis développeur principalement frontend (Vuejs) et je fais aussi du backend. De ce côté, j'ai de l'expérience en PHP avec Laravel, mais j'ai aussi touché à du .NET à quelques reprises. Je peux aussi créer des scripts CI et j'ai de l'expérience avec Docker.",
   },
   {
     id: "step1",
     title: "Étape 1: Rencontre initiale",
     message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Durant cette rencontre d'environ 1h, on prend le temps de regarder les besoins et les délais. ",
   },
   {
     id: "step2",
     title: "Étape 2: Envoi de la soumission",
     message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Après s'être mis d'accord sur les besoins, les délais et les conditions, je vous soumettrai une soumission qui décrit les travaux à faire avec le prix et le nombre d'heures requis.",
   },
   {
     id: "step3",
     title: "Étape 3: Développement",
     message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Sur acception de la soumission et des conditions, le développement s'en suivra et un suivi avec vous durant l'avancement du projet sera possible selon votre préférence.",
   },
   {
     id: "step4",
-    title: "Étape 4: Derniers ajustements et livraison",
+    title: "Livraison",
     message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Suite à la fin du développement, une présentation finale sera faite du produit final et certains ajustements pourront être apportés au besoin avant le lancement.",
   },
 ];
