033a5ec9221865b71ec61da26fa98e229ab34cbc
Hugo Lemieux <52041544+hughockey@users.noreply.github.com>
2024-09-13 09:23:10 -0400

    add footer (#6)

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
 import Content from './components/Content.vue';
+import Footer from './components/Footer.vue';
 import { ref } from 'vue';
 
 const content = ref<InstanceType<typeof Content> | null>(null)
 const scrollToSection = () => {
-  console.log(content.value?.$refs)
   const section = content.value?.$refs['contact-section'] as HTMLElement | undefined
   if (section) {
     section.scrollIntoView({behavior: 'smooth'})
   }
-
 }
 </script>
 
 <template>
   <Header @scroll-to-section="scrollToSection"></Header>
   <Content ref="content"></Content>
-
+  <Footer></Footer>
 </template>
 
 <style scoped>
