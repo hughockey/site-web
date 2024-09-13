f403455ad000db733d32a766f819a34a87968fc0
Hugo Lemieux <h.lemieux66@gmail.com>
2024-09-13 09:22:38 -0400

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="footer-container">
+    <span>Hugo Lemieux &#169;2024</span>
+  </div>
+</template>
+<style lang="css" scoped>
+  .footer-container {
+    height: 40px;
+    text-align: center;
+    text-transform: uppercase;
+    font-family: var(--font-text);
+  }
+</style>
