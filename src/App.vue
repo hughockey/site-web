e8caca26b084ea23a0b4a3e526bef94a0bd90aa7
Hugo Lemieux <h.lemieux66@gmail.com>
2025-02-10 20:20:31 -0500

    ensure heading behaves correctly when change page

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,18 @@
 import Header from "./components/Header.vue";
 import Content from "./components/Content.vue";
 import Footer from "./components/Footer.vue";
-import { onMounted, ref } from "vue";
+import { nextTick, onMounted, ref } from "vue";
 import Projects from "./components/Projects.vue";
 
-const content = ref<InstanceType<typeof Content> | null>(null);
-const scrollToSection = () => {
-  const section = content.value?.$refs["contact-section"] as
-    | HTMLElement
-    | undefined;
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-};
-
 const currentRoute = ref(window.location.pathname);
+const content = ref<InstanceType<typeof Content> | null>(null);
+const scrollToSection = async () => {
+  if (window.location.pathname !== '/') window.history.pushState({}, "", '/');
+  currentRoute.value = '/';
+  await nextTick()
+  const section = content.value?.$refs["contact-section"] as | HTMLElement
+  section.scrollIntoView({ behavior: "smooth" });
+};
 
 const handleNavigation = (path: string) => {
   if (currentRoute.value !== path) {
@@ -32,7 +30,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <Header @scroll-to-section="scrollToSection" @navigate="handleNavigation"></Header>
+  <Header :current-route="currentRoute"  @scroll-to-section="scrollToSection" @navigate="handleNavigation"></Header>
   <div v-if="currentRoute === '/'">
     <Content ref="content"></Content>
   </div>
